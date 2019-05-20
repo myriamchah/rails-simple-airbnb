9d081b9296b1279760fabcd59315c3dbc8d324f0
Myriam Chahlafi <myriam.chah@gmail.com>
2019-05-20 17:32:15 +0200

    index new show views with some front to be improved

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,35 @@
 class FlatsController < ApplicationController
+  before_action :set_current_flat
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    set_current_flat
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :description, :address, :price_per_night, :number_of_guests)
+  end
+
+  def set_current_flat
+    if params[:id]
+      @flat = Flat.find(params[:id])
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Lovely Castle',
+  address: 'Burningham Palace',
+  description: 'Plenty of bedrooms & a big garden!',
+  price_per_night: 300,
+  number_of_guests: 10
+)
+
+Flat.create!(
+  name: 'Pretty nice flat for 2',
+  address: '10 Downing Street',
+  description: 'A little but cosy and functional flat',
+  price_per_night: 50,
+  number_of_guests: 2
+)
